c4dec586cacd3264773d3d56227db7eca9d13aca
Amulek1416 <29697968+Amulek1416@users.noreply.github.com>
2020-08-27 07:02:33 -0600

    Added file header

--- PWM-lib/PWM-lib.ino
+++ PWM-lib/PWM-lib.ino
@@ -1,6 +1,18 @@
 /**
+ * @file    PWM-lib.ino
+ * @date    Aug 26 2020
+ * @author  Amulek1416
  * 
+ * @brief   File for testing and debugging components 
+ *          of the library. If everything in this file
+ *          works on the board, then the board has been
+ *          fully supported.
  * 
+ * @warning This is still a working progress. Once the 
+ *          first board (Arduino Uno) is fully implemented, 
+ *          there will be more testing functions created. 
+ *          Right now this is just used to make sure it 
+ *          compiles and there are no syntax errors.
  */
 
 #include "PWM.h"
